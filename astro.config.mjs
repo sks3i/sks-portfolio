09bcea7d2f4239a819684301655d4bcddc385bba
Sabarish Kuduwa Sivanath <saba_rish91@hotmail.com>
2026-02-06 09:44:38 +0000

    fix website path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://djsiddz.github.io",
-  base: "/space-ahead",
+  site: "https://sabarishks.com",
   integrations: [
     swup({
       theme: ["overlay", { direction: "to-top" }],
