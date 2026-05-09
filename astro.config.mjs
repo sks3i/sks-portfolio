0deebbda5be7553dc50f3aabb09e1203648e9214
Sabarish Kuduwa Sivanath <saba_rish91@hotmail.com>
2026-05-08 20:14:24 -0700

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const isUserSiteRepo = repo.endsWith('.github.io');
+const base = isGitHubActions && repo && !isUserSiteRepo ? `/${repo}` : '/';
+const site = process.env.GITHUB_REPOSITORY_OWNER
+	? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io`
+	: undefined;
+
+export default defineConfig({
+	site,
+	base,
+});
