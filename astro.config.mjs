eed663f42e716e442eb757d42841c8b7929eea6d
Sabarish Kuduwa Sivanath <saba_rish91@hotmail.com>
2026-05-08 20:31:49 -0700

    something

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,17 @@ import { defineConfig } from 'astro/config';
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
 const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
 const isUserSiteRepo = repo.endsWith('.github.io');
-const base = isGitHubActions && repo && !isUserSiteRepo ? `/${repo}` : '/';
-const site = process.env.GITHUB_REPOSITORY_OWNER
-	? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io`
-	: undefined;
+const customDomain = process.env.CUSTOM_DOMAIN;
+
+const base = customDomain ? '/' : isGitHubActions && repo && !isUserSiteRepo ? `/${repo}` : '/';
+
+const site = customDomain
+	? `https://${customDomain}`
+	: process.env.GITHUB_REPOSITORY_OWNER
+		? isUserSiteRepo
+			? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io`
+			: `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${repo}`
+		: undefined;
 
 export default defineConfig({
 	site,
